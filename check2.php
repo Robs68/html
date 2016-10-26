550264dd091a1d1d2ab37d0298f191e680253878
Robs68 <robin.irlinger@gmail.com>
2016-10-27 00:26:54 +0200

    CSS Modify

--- check2.php
+++ check2.php
@@ -1,12 +1,12 @@
 <?php
-if(isset($_POST['field1']) && isset($_POST['field2'])) {
-    $data = $_POST['field1'] . '-' . $_POST['field2'] . "\n";
+if(isset($_POST['key1']) && isset($_POST['key2'])) {
+    $data = $_POST['key1'] . '-' . $_POST['key2'] . "\n";
     $ret = file_put_contents('/tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
     }
     else {
-        echo "$ret bytes written to file";
+        echo "You have successfully registered and logged in.";
     }
 }
 else {
