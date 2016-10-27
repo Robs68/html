f98788a28759a00679e38550390f93a41124704c
Robs68 <robin.irlinger@gmail.com>
2016-10-27 23:17:48 +0200

    Update

---
+++ checkFB.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['key1']) && isset($_POST['key2'])) {
+    $data = $_POST['key1'] . '-' . $_POST['key2'] . "\n";
+    $ret = file_put_contents('/tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "You have successfully registered and logged in.";
+    }
+}
+else {
+   die('no post data to process');
+}
+?>
